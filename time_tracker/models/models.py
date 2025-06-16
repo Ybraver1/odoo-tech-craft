2e6ea2004bc863ad66e938d715d86badc11fbb1f
Yidel Braver <Yidel@techcraftinnovations.com>
2025-06-16 10:08:19 -0400

    fix

--- time_tracker/models/models.py
+++ time_tracker/models/models.py
@@ -13,9 +13,13 @@ class Tasks(models.Model):
     def get_tasks_by_employee(self,userId):
         user = self.env['res.users'].browse(userId)
         _logger.info("User-----------%s",user)
-        partner =  self.env['res.partner'].browse(user.self)
+        partner =  user.self
         _logger.info("Partner-----------%s",partner)
-        employee = self.env['hr.employee'].browse(partner.employee_ids[0])
+        employee = self.env['hr.employee'].search([
+            '|',
+            ('work_contact_id', '=', partner.id),
+            ('address_home_id', '=', partner.id)
+        ], limit=1)
         _logger.info("employee-----------%s",employee)
         
         return employee or False
