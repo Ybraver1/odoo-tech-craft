277d6f5d45f5299aa4c03152cbb8dbab0cd79b19
Yidel Braver <Yidel@techcraftinnovations.com>
2025-06-16 10:12:05 -0400

    added sudo

--- time_tracker/models/models.py
+++ time_tracker/models/models.py
@@ -15,7 +15,7 @@ class Tasks(models.Model):
         _logger.info("User-----------%s",user)
         partner =  user.self
         _logger.info("Partner-----------%s",partner)
-        employee = self.env['hr.employee'].search([
+        employee = self.env['hr.employee'].sudo().search([
             '|',
             ('work_contact_id', '=', partner.id),
             ('address_home_id', '=', partner.id)
