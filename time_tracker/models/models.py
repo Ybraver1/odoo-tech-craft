cfdc4196e0b5cda878f169cb9ce34f23a92eea24
Yidel Braver <Yidel@techcraftinnovations.com>
2025-06-16 13:02:50 -0400

    added create timesheet

--- time_tracker/models/models.py
+++ time_tracker/models/models.py
@@ -14,5 +14,20 @@ class Tasks(models.Model):
         ], limit=1)       
         tasks = self.sudo().search([('employee', '=', employee.id)]) 
         return tasks.read(['id','name','project_id']) or False
+    
+    @api.model
+    def create_time_sheet(self,task_id,time):
         
+        task = self.sudo().browse(task_id)
+        
+        vals = {
+            'name': f'Timesheet for task {task.name}',
+            'project_id': task.project_id.id,
+            'task_id': task.id,
+            'employee_id': task.employee.id,
+            'unit_amount': time,  # e.g. 2.5 = 2.5 hours
+            'date': fields.Date.today(),
+        }
+        timesheet = self.env['account.analytic.line'].sudo().create(vals)
+        return timesheet.id
   
