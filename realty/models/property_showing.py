60f2f181ccddbada2087605e3fd427983e5ea384
Yidel Braver <Yidel@techcraftinnovations.com>
2025-06-18 13:45:15 -0400

    update

--- realty/models/property_showing.py
+++ realty/models/property_showing.py
@@ -9,3 +9,8 @@ class PropertyShowing(models.Model):
     scheduled_showing = fields.Datetime()
     offer = fields.Float()
     stage_id = fields.Many2one("realty.property_showing_stage",tracking=True)
+    name= fields.Char(compute='_compute_name')
+    
+    def _compute_name(self):
+        for rec in self:
+            rec.name = rec.client_id.name if rec.client_id else 'Showing'
