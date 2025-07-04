a7fac1e0e70f288da1c906dc34747c74a28c7c24
Yidel Braver <Yidel@techcraftinnovations.com>
2025-07-04 09:54:25 -0400

    update

--- time_tracker/models/hr_employee.py
+++ time_tracker/models/hr_employee.py
@@ -1,5 +1,5 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
+from .wise_api import WiseAPI
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
@@ -7,3 +7,29 @@ class HrEmployee(models.Model):
     wise_currency = fields.Char(default='USD')
     wise_recipient_id = fields.Char()
     
+    
+    def create_wise_recipient(self):
+        token = self.env['ir.config_parameter'].sudo().get_param('wise.api_key')
+        profile_id = self.env['ir.config_parameter'].sudo().get_param('wise.profile_id')
+        for employee in self:
+            if employee.wise_email and not employee.wise_recipient_id:
+                # Call Wise API to create recipient
+                wise_api = WiseAPI(token, profile_id)
+                recipient = wise_api.create_recipient(
+                    account_holder_name=employee.name,
+                    email=employee.wise_email,
+                    currency=employee.wise_currency
+                )
+                employee.wise_recipient_id = recipient.get('id')
+
+
+    @api.model
+    def create(self, vals):
+        employee = super().create(vals)
+        employee.create_wise_recipient()
+        return employee
+    
+    def write(self, vals):
+        res = super().write(vals)
+        self.create_wise_recipient()
+        return res
