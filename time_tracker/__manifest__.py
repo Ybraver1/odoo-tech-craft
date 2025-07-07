5bfb7c5973039af6da67c601df406a3623741cee
Yidel Braver <Yidel@techcraftinnovations.com>
2025-07-07 12:22:54 -0400

    update

--- time_tracker/__manifest__.py
+++ time_tracker/__manifest__.py
@@ -26,7 +26,7 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/res_partner_views.xml',
